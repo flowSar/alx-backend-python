6ca48c1490f14dd964c27f5302abd88045aea8e4
flowsar <brahim.sar151@gmail.com>
2024-10-10 22:41:16 +0100

    multipl

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """nested functions for calc multiplier"""
+    def multiply_by_multiplier(value: float) -> float:
+        return value * multiplier
+    return multiply_by_multiplier
