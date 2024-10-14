cae97fce065fc853475ebc8682bf70f48930a591
flowsar <brahim.sar151@gmail.com>
2024-10-14 23:16:10 +0100

    measures the total execution time

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,1 +1,15 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
+"""measures the total execution time"""
+import time
+import asyncio
+from previous_file import wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """measures the total execution time"""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    return total_time / n
