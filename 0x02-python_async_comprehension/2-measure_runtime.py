47c3567c7bad1715917f001db0ddfe83450bb510
flowsar <brahim.sar151@gmail.com>
2024-10-15 23:34:11 +0100

    Run time for four parallel comprehensions

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Run time for four parallel comprehensions"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_generator
+
+async def measure_runtime() -> float:
+    """Run time for four parallel comprehensions"""
+    start_time = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = time.time()
+    
+    total_runtime = end_time - start_time
+    return total_runtime
