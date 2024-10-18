48827de6b733f6bf47478082ac8e4b263a428a8e
flowsar <brahim.sar151@gmail.com>
2024-10-18 15:27:27 +0100

    create a task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """task_wait_random"""
 import asyncio
-from 0_basic_async_syntax import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """task_wait_random"""
+    """create and return a task"""
     task = asyncio.create_task(wait_random(max_delay))
     return task
--- 0x01-python_async_function/test.py
+++ 0x01-python_async_function/test.py
@@ -3,24 +3,20 @@
 import time
 import asyncio
 import time
-wait_n = __import__('1-concurrent_coroutines').wait_n
 
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-def measure_time(n: int, max_delay: int) -> float:
-    """measures the total execution time"""
-    start_time = time.time()
-    asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-    total_time = end_time - start_time
-    return total_time/n
-
-n = 5
-max_delay = 9
-
-print(measure_time(n, max_delay))
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
 
 
 
 
 
+async def test(max_delay: int) -> float:
+    task = task_wait_random(max_delay)
+    await task
+    print(task.__class__)
 
+asyncio.run(test(5))
