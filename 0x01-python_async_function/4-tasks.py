4baf3ea0d413cfca557b4f0ed68844a852719516
flowsar <brahim.sar151@gmail.com>
2024-10-18 15:36:58 +0100

    get the list of deplay in ascend order

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
+"""get the list of deplay in ascend order"""
+import random
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
 
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """get the list of deplay in ascend order"""
+    delays: List[float] = []
+    tasks: List = []
+
+    for _ in range(n):
+        tasks.append(task_wait_random(max_delay))
+
+    for task in asyncio.as_completed((tasks)):
+        delays.append(await task)
+
+    return delays
